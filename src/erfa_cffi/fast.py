d0d0bae0780e24b53ca2e92920c50016661d6e51
Juan Luis Cano Rodríguez <juanlu@satellogic.com>
2020-05-20 17:20:02 +0200

    Add vectorized example

--- src/erfa_cffi/fast.py
+++ src/erfa_cffi/fast.py
@@ -1,5 +1,6 @@
-from numba import njit
+from numba import njit, vectorize
 from numba.core.typing.cffi_utils import register_module
+from numba.types import float64
 
 import _erfa_cffi
 
@@ -10,4 +11,9 @@ register_module(_erfa_cffi)
 
 @njit
 def eraEpb(dj1, dj2):
+    return _eraEpb(dj1, dj2)
+
+
+@vectorize([float64(float64, float64)])
+def eraEpb_v(dj1, dj2):
     return _eraEpb(dj1, dj2)
